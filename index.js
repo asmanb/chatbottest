39148d192ceda8c6765635043d488226fedf211f
munwrite <asman.baheh@outlook.com>
2018-06-15 03:18:12 +0700

    idk

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-'use strict'
+'use strict';
 
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
 
-const app = express()
-
-app.set('port', (process.env, port || 5000))
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
-
-
-app.get('/', function(req,res){
-
-res.send("HI")
-})
-
-
-app.get('/webhook', (req, res) => {
+  app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "mwritetk"
+    let VERIFY_TOKEN = "asmanbaheh"
       
     // Parse the query params
     let mode = req.query['hub.mode'];
@@ -47,8 +61,6 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
-  
-  app.listen(app.get('port'),function(){
-    console.log("running port")
-  })
 
+
+  
