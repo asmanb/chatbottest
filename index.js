0a5822439d94766a0fbf9815d0555c4dcbe31a83
munwrite <asman.baheh@outlook.com>
2018-06-15 06:01:36 +0700

    reset again

--- index.js
+++ index.js
@@ -2,101 +2,66 @@
 
 // Imports dependencies and set up http server
 const
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json()); // creates express http server
-
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 
-
-
-
-app.get('/', function (req, res) {
-    res.send("Hi I am a chatbot 555 ")
-});
-
-
-
-app.post('/webhook', (req, res) => {
-
-    // Parse the request body from the POST
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
     let body = req.body;
-
-    // Check the webhook event is from a Page subscription
+  
+    // Checks this is an event from a page subscription
     if (body.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
-
-            // Gets the body of the webhook event
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-
-            // Get the sender PSID
-            let sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid+'THat');
-
-        });
-
-        // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
-
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
     } else {
-        // Return a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
     }
+  
+  });
 
-});
 
 
-// Adds support for GET requests to our webhook
+  // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
     let VERIFY_TOKEN = "asmanbaheh"
-
+      
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-
+      
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
     }
-});
-
-
-
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-
-}
+  });
