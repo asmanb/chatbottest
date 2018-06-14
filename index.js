40855d731f35bfa91e39be583d1f8a97e5666965
munwrite <asman.baheh@outlook.com>
2018-06-15 06:57:02 +0700

    comment all function and calling condition

--- index.js
+++ index.js
@@ -33,14 +33,14 @@ app.post('/webhook', (req, res) => {
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
 
-
+/*
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
-            }
+            } */
 
         });
 
@@ -83,7 +83,7 @@ app.get('/webhook', (req, res) => {
     }
 });
 
-
+/*
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
@@ -133,4 +133,4 @@ function callSendAPI(sender_psid, response) {
             console.error("Unable to send message:" + err);
         }
     });
-}
+}*/
