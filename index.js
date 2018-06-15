840a07c030f05190195ccc25740029c2af610718
munwrite <asman.baheh@outlook.com>
2018-06-15 09:28:45 +0700

    add struct msg

--- index.js
+++ index.js
@@ -104,11 +104,39 @@ function handleMessage(sender_psid, received_message) {
 
         // Gets the URL of the message attachment
         let attachment_url = received_message.attachments[0].payload.url;
-
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
+    
+    
+    
+    }
     //}
    // else {
        // console.log('msg is NOT reci');
-    }
+    
 
     // Sends the response message
     callSendAPI(sender_psid, response);
